166940f356a63932d8c533e2e840802e54300be8
Oleg <Ohrimus@yandex.ru>
2017-05-30 15:41:59 +0300

    All project

---
+++ config/db_params.php
@@ -0,0 +1,16 @@
+<?php
+
+
+return array(
+    'host' => 'tanzor.mysql.ukraine.com.ua',
+    'dbname' => 'tanzor_supermag',
+    'user' => 'tanzor_supermag',
+    'password' => 'bjs8ekpd',
+);
+
+/*return array(
+    'host' => 'localhost',
+    'dbname' => 'phpshop',
+    'user' => 'phpstart',
+    'password' => '',
+);*/
